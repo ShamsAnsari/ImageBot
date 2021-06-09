12e6af2dcabe2d6ffb04ff1c8150c4dc044ba560
Shams Ansari <shamsahmedansari@gmail.com>
2021-06-09 06:17:36 -0700

    first commit

--- misc.py
+++ misc.py
@@ -12,10 +12,16 @@ class Misc(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         description = f'[:robot: Invite me!]({os.environ.get("INVITE")})\n[:information_source: Github Repo]({self.repolink})'
+
+
         e = discord.Embed(color=discord.Color.purple(), title="Help Menu", description=description)
+
+        # grab command
         e.add_field(name="*grab",
                     value="Type \\*grab <optional num> followed by a couple words to search for an image.",
                     inline=False)
+        e.add_field(name="*grabpp", value="Type \"*grabpp\" and @ (mention) someone to get their profile picture.")
+        # help command
         e.add_field(name="*help", value="Type \"*help\" for the help menu", inline=False)
         e.set_image(url="https://media.giphy.com/media/Lt5eOpMHfyHveYg8Bu/giphy.gif")
         await ctx.send(embed=e)
