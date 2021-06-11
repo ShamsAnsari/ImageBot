909dcb2b0d1e4d9eed91f77705f2de70d8cc59a4
Shams Ansari <shamsahmedansari@gmail.com>
2021-06-11 09:27:36 -0700

    None

--- grabber.py
+++ grabber.py
@@ -1,11 +1,12 @@
 import random
+import re
 
 import discord
 from discord.ext import commands
-
+import cv2
 import duckduckgo
-import imgsearch
 import logger
+
 
 
 class Grabber(commands.Cog):
@@ -21,48 +22,19 @@ class Grabber(commands.Cog):
         :param query: A string describing the image
         :return: None
         """
-        result_num, query = self.get_image_num(query)
-        print(result_num, query)
-        query = imgsearch.clean_query(query)
-        if query == "":
-            return await self.send_error(ctx)
+        result_num = self.get_num(query)
+        query = Grabber.clean_query(Grabber.clean_brackets(query))
+
+        if query == "": return await self.send_error(ctx)
 
         img_url = duckduckgo.search(query, result_num)
-        if img_url == None:
-            return await self.send_error(ctx)
+
+        if img_url is None: return await self.send_error(ctx)
 
         e = discord.Embed(color=discord.Color.purple(), title=query)
         e.set_image(url=img_url)
         await ctx.send(embed=e)
-
         self.log.log_command_wrapper(ctx, img_url)
-
-    def get_image_num(self, msg):
-        '''
-        Gets number inside < > brackets
-        :param msg:
-        :return:
-        '''
-        if msg.find("<") >= 0 and msg.find(">") >= 0:
-            num = msg[msg.find('<') + 1:msg.find('>')]
-            if num.isdigit():
-                return int(num), (msg[:msg.find('<')] + msg[msg.find('>') + 1:]).strip()
-        return 0, msg
-
-    async def send_error(self, ctx):
-        """
-        Sends an image of a funny cat if there is no image found.
-        Codes are error codes on website
-        :param ctx:
-        :return:
-        """
-        codes = [100, 101, 102, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402,
-                 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424,
-                 425, 426, 429, 431, 444, 450, 499, 500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599]
-        await ctx.send(f'<@{ctx.author.id}>, No image to grab!')
-        e = discord.Embed(color=discord.Color.purple())
-        e.set_image(url=f'https://http.cat/{codes[random.randrange(0, len(codes))]}')
-        return await ctx.send(embed=e)
 
     @commands.command()
     async def grabpp(self, ctx, *, user: discord.User = None):
@@ -79,14 +51,50 @@ class Grabber(commands.Cog):
         e.set_image(url=image)
         await ctx.send(embed=e)
 
-    # @commands.command()
-    # async def scramblepp(self, ctx, *, user: discord.User = None):
-    #     if user is None:
-    #         user = ctx.author
-    #     image = user.avatar_url
-    #     e = discord.Embed(color=discord.Color.purple(), title=f'{user.display_name}\'s PP')
-    #     e.set_image(url=image)
-    #     await ctx.send(embed=e)
+
+    @staticmethod
+    def get_num(msg):
+        """
+        Gets number inside < > brackets
+        :param msg:
+        :return:
+        """
+        if msg.find("<") >= 0 and msg.find(">") >= 0:
+            num = msg[msg.find('<') + 1:msg.find('>')]
+            if num.isdigit():
+                return int(num)
+        return 0
+
+    @staticmethod
+    async def send_error(ctx):
+        """
+        Sends an image of a funny cat if there is no image found.
+        Codes are error codes on website
+        :param ctx:
+        :return:
+        """
+        codes = [100, 101, 102, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402,
+                 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424,
+                 425, 426, 429, 431, 444, 450, 499, 500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599]
+        await ctx.send(f'<@{ctx.author.id}>, No image to grab!')
+        e = discord.Embed(color=discord.Color.purple())
+        e.set_image(url=f'https://http.cat/{codes[random.randrange(0, len(codes))]}')
+        return await ctx.send(embed=e)
+
+    @staticmethod
+    def clean_query(query):
+        """
+          Cleans the query, remove any troublesome characters
+        """
+        query = query.encode("ascii", "ignore").decode()
+        query = re.sub(r'[^a-zA-Z0-9 ]', '', query).strip()
+        return query
+
+    @staticmethod
+    def clean_brackets(msg):
+        if msg.find("<") >= 0 and msg.find(">") >= 0:
+            return (msg[:msg.find('<')] + msg[msg.find('>') + 1:]).strip()
+        return msg
 
 
 def setup(bot):
--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from discord.ext import commands
@@ -12,10 +13,16 @@ class Misc(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         description = f'[:robot: Invite me!]({os.environ.get("INVITE")})\n[:information_source: Github Repo]({self.repolink})'
+
         e = discord.Embed(color=discord.Color.purple(), title="Help Menu", description=description)
+
+        # grab command
         e.add_field(name="*grab",
                     value="Type \\*grab <optional num> followed by a couple words to search for an image.",
                     inline=False)
+        # grabpp
+        e.add_field(name="*grabpp", value="Type \"*grabpp\" and @ (mention) someone to get their profile picture.")
+        # help command
         e.add_field(name="*help", value="Type \"*help\" for the help menu", inline=False)
         e.set_image(url="https://media.giphy.com/media/Lt5eOpMHfyHveYg8Bu/giphy.gif")
         await ctx.send(embed=e)
